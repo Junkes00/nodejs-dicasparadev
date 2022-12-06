bf9ed59b0f6e33b3742f13aa4180b261ff71f124
Nicolas Junkes <nicolas.junkes00@gmail.com>
2022-12-06 00:58:22 -0300

    Use path method to get file informations

--- modules/path.js
+++ modules/path.js
@@ -1,5 +1,16 @@
-import path from "path";
+const path = require("path");
 
-// Basename
+// Only current file name
 console.log(path.basename(__filename));
 
+// Name of current path/directory
+console.log(path.dirname(__filename));
+
+// File extention
+console.log(path.extname(__filename));
+
+// Create an object path
+console.log(path.parse(__filename));
+
+// Merge file paths
+console.log(path.join(__dirname, "/teste", "teste.html"));
